811b632af64096dfceb00e433ae7fc1b3b4356be
Matthew Caldwell <caldwellysr@gmail.com>
2017-01-31 11:15:41 -0500

    add createGame function that handles cloning the repo and changing the remote

--- index.js
+++ index.js
@@ -3,14 +3,65 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const CLI = require('clui');
-const figconst = require('figconst');
+const figlet = require('figlet');
 const inquirer = require('inquirer');
 const Preferences = require('preferences');
 const Spinner = CLI.Spinner;
 const GitHubAPI = require('github');
 const _ = require('lodash');
-const git = require('simple-git')();
+let git = require('simple-git')();
 const touch = require('touch');
 const fs = require('fs');
+const files = require('./lib/files');
+const starterProject = 'https://github.com/SplatJS/splat-ecs-starter-project.git';
 
+function createGame(next) {
+    const args = require('minimist')(process.argv.slice(2));
 
+    const questions = [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter a name for your game: ',
+            default: args.name || args.n || files.getCurrentDirectoryBase(),
+            validate: (value) => {
+                if (value.length) {
+                    return true;
+                } else {
+                    return "Please enter a name for your game";
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'repo',
+            message: 'Git Repo: ',
+            default: args.r || args.repo
+        }
+    ];
+
+    inquirer.prompt(questions).then((answers) => {
+        let status = new Spinner('Creating game...');
+        status.start();
+        let output;
+        try {
+            output = git.clone(starterProject, answers.name);
+        } catch(err) {
+            return next(err)
+        }
+        status.stop();
+        return next(answers.repo, output._baseDir + "/" + answers.name);
+    });
+}
+
+createGame((remote, gitDir) => {
+    let status = new Spinner('Updating repo...');
+    status.start();
+    let git = require('simple-git')(gitDir);
+    git.removeRemote("origin");
+    if (remote.length) {
+        git.addRemote("origin", remote);
+    }
+    status.stop();
+    return true;
+});
